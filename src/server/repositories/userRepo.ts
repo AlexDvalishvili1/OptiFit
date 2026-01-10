25af1498488a5bf9db89f9725283225cfeaeed7c
AlexDvalishvili1 <alex-dvalishvili@mail.ru>
2026-01-10 04:22:49 +0400

    Added connecting to DB for db actions

--- src/server/repositories/userRepo.ts
+++ src/server/repositories/userRepo.ts
@@ -3,8 +3,10 @@ export const dynamic = "force-dynamic";
 
 import {User, type UserSchema} from "@/server/models/User";
 import type {UpdateQuery, QueryOptions} from "mongoose";
+import {connectDB} from "@/server/db/connect";
 
 export async function findUserLeanById(id: string) {
+    await connectDB();
     return User.findById(id).lean<UserSchema>();
 }
 
@@ -13,5 +15,6 @@ export async function updateUserById(
     update: UpdateQuery<UserSchema>,
     options: QueryOptions<UserSchema> = {}
 ) {
+    await connectDB();
     return User.findByIdAndUpdate(id, update, options);
 }
